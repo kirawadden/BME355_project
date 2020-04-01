5e1fbf4b95ca5072068ed448471900cdbbafa65e
kirawadden <kirawa6@gmail.com>
2020-04-01 09:51:04 -0700

    more cleanup

--- ankle_model.py
+++ ankle_model.py
@@ -1,8 +1,7 @@
-import numpy as np
 import math
 import matplotlib.pyplot as plt
+import numpy as np
 import scipy.integrate
-import random
 from data import Data
 
 class FootDropAnkleModel:
@@ -31,10 +30,12 @@ class FootDropAnkleModel:
         self.muscle_tendon_length_rest = 0.321 # m --> original 32.1 # lmt,0 -> cm
         self.elastic_torque_params = [2.1, -0.08, -7.97, 0.19, -1.79] # [a1, a2, a3, a4], Tela
   
+
     def get_current_muscle_excitation(self, t):
         for i in range(len(Data.muscle_excitation_level_fig3)):
             if Data.muscle_excitation_level_fig3[i][0] > t:
                 return Data.muscle_excitation_level_fig3[i][1]
+
 
     def get_current_ext_vector(self, x_ext, t):
         """
@@ -42,17 +43,15 @@ class FootDropAnkleModel:
         :param x_ext: 2D array containing time poins and data points for external state vecctors
         :return data value of external state vector closest to passed in time point: 
         """
-        # TODO: return value closer to time vs. just at i
-        # TODO: ensure that this works properly
         # iterate through all data points in corresponding data array
-        t += 0.55 ### added 0.55 to time as we believe this is where the swing phase starts
-                  ### there's a pretty drastic magnitude different if we reduce to t += 0.5
+        # swing phase starts for external state vector at 0.55s, so time is adjusted to this point 
+        # in simulation
+        t += 0.55
         for i in range(len(x_ext)-1):
             if x_ext[i][0] > t:
                 return x_ext[i-1][1]
-    
-  
-  #eqn 4
+
+
     def compute_gravity_torque(self, x2):
         """
         :param x2: alpha, absolute orientation of foot wrt horizontal axis 
@@ -62,7 +61,6 @@ class FootDropAnkleModel:
         return -(self.foot_mass * self.centre_of_foot_mass_wrt_ankle * math.cos(x2) * g)
 
 
-  # eqn 5
     def compute_acceleration_torque(self, x2, x_ext1, x_ext2):
         """
         :param x2: alpha, absolute orientation of foot wrt horizontal axis 
@@ -70,7 +68,8 @@ class FootDropAnkleModel:
         :param x_ext2: linear acceleration of foot in vertical direction
         :return: torque induced by the movement of the ankle
         """
-        return self.foot_mass * self.centre_of_foot_mass_wrt_ankle * (x_ext1 * math.sin(x2) - x_ext2 * math.cos(x2))
+        return self.foot_mass * self.centre_of_foot_mass_wrt_ankle * (x_ext1 * math.sin(x2) - 
+               x_ext2 * math.cos(x2))
 
 
     def compute_force_velocity(self, x3, x_ext4):
@@ -92,7 +91,6 @@ class FootDropAnkleModel:
             return (1 + (self.av_param*(velocity_ce/self.fv2)))/(1 + (velocity_ce/self.fv2))
 
 
-    # eqn 7
     def compute_ta_muscular_force(self, x1, x2, x3, x_ext3, x_ext4):
         """
         :param x1: dyanimc activation level of foot
@@ -102,11 +100,12 @@ class FootDropAnkleModel:
         :param x_ext4: absolute velocity rotation of the shank
         :return: tibialus anterior muscular force
         """
-        # non-linear relationship linking generated force to length of muscle and therefore to ankle joint angle
+        # non-linear relationship linking generated force to length of muscle and therefore 
+        # to ankle joint angle
         length_muscle_tendon = self.muscle_tendon_length_rest - self.ta_moment_arm_wrt_ankle * (x_ext3 - x2)
         length_ce =  length_muscle_tendon - self.const_tendon_length
 
-        length_ce_opt = 0.321 # 32.1 [cm] -> m
+        length_ce_opt = 0.321 # m
         f_fl_eqn = (length_ce - length_ce_opt) / (self.shape_param*length_ce_opt)
         f_fl = math.exp(-math.pow(f_fl_eqn,2))
         f_fv = self.compute_force_velocity(x3, x_ext4)
@@ -114,12 +113,12 @@ class FootDropAnkleModel:
         return x1 * self.max_isometric_force * f_fl * f_fv 
 
 
-    # eqn 6
     def compute_passive_elastic_torque(self, x2):
         """
         :param x2: alpha, absolute orientation of foot wrt horizontal axis 
         :return: passive elastic torque around the ankle
-        :note: it is assumed that the knee angle variation is not significant to the ankle angular range 
+        :note: it is assumed that the knee angle variation is not significant to the ankle angular 
+        range 
         """
         a1 = self.elastic_torque_params[0]
         a2 = self.elastic_torque_params[1]
@@ -129,11 +128,12 @@ class FootDropAnkleModel:
 
         return math.exp(a1+a2*x2) - math.exp(a3+a4*x2) + a5
 
-    # state variable eqns 1, 2, 3
+
     def compute_state_vector_derivative(self, t, x):
         """
         :param t: time
-        :param x: state vector [dyanimc activation level of foot, abs orientation of foot, abs rotational velocity of foot]
+        :param x: state vector [dyanimc activation level of foot, abs orientation of foot, abs 
+        rotational velocity of foot]
         :return: derivative of state vector
         """
         x1 = x[0]
@@ -143,8 +143,8 @@ class FootDropAnkleModel:
         # external state vector
         x_ext1 = self.get_current_ext_vector(Data.linear_acc_shank_x, t)
         x_ext2 = self.get_current_ext_vector(Data.linear_acc_shank_z, t)
-        x_ext3 = self.get_current_ext_vector(Data.abs_orientation_shank, t) #* (math.pi/180)
-        x_ext4 = self.get_current_ext_vector(Data.abs_velocity_rotation_shank, t) # * (180/math.pi) ### might need unit conversion but also makes the graphs look like they resonate...
+        x_ext3 = self.get_current_ext_vector(Data.abs_orientation_shank, t) 
+        x_ext4 = self.get_current_ext_vector(Data.abs_velocity_rotation_shank, t) 
 
         # get parameters needed to calculate derivatives
         Fm = self.compute_ta_muscular_force(x1, x2, x3, x_ext3, x_ext4)
@@ -161,44 +161,12 @@ class FootDropAnkleModel:
         # return derivative state vector
         return np.array([dt_x1, dt_x2, dt_x3])
 
+
     def simulate(self, sim_duration):
         """
         :param sim_duration: duration of the simulation in seconds
         :return: solution to state derivative IVP
         """
         x_init = [0, -15, 0]  
-        sol = scipy.integrate.solve_ivp(self.compute_state_vector_derivative, [0.01, sim_duration], x_init, method='RK45', max_step = 0.01)
+        sol = scipy.integrate.solve_ivp(self.compute_state_vector_derivative, [0.01, sim_duration], x_init, method='RK45', max_step=0.01)
         return sol
-
-    """
-    Cannot start the simulation at 0. This is because the u input gets caught on the first value if we start at 0.
-    The u vector data is only for the swing phase (.36 seconds). This is from the main paper.
-    The main paper abs. orientation of shank ranges -15 to about +15 Figure 2
-    The data paper abs. orientation of shank ranges -15 to about +50 Figure 6 a   
-    Note, looking at data paper, abs. orientation of shank increases with stimulation. Normal walking has greatest range. 
-    Therefore, we expect differences, seeing that the input data and euqations have different outputs.  *** angle orient. different expected values
-    Current biggest concern: There is a big spike at the beginning of msucle activation (fact) - fig 3 graph c).
-    We currently are not getting this big spike when we run the simulation. We think that the activation trend for our sim
-    should following the exciation input vector.
-    We think that it will be in our best interest if everyone can take a look at the code
-    Thing we can look at:
-    - Units (specifically radians vs degrees)
-    - Double check all equations and constants
-    - Possibly look at time step for equation solver
-    - Check input data and verify that it matches the graphs that we extracted it fro
-    - Verify that the methods being used to select the 'current' inputs (u and x_ext values) is correct
-    Notes:
-    - code looks difficult to read, but actually isn't too bad
-    - Graphing happens in main.py, class methods are in ankle_model and data is in data
-    - We are using muscle_excitation_level_fig3
-    - Run the project by going to the file that you are storing this project in (in your terminal). Then type 'python3 main.py' into your terminal/command line.
-    - We are running the sim for 0.35s as that is what they do in the paper, and the time length of the input vector (u).
-        - We need to ensure that the data we are using from the external vector graphs corresponds to the swing phase (I think it start around 0.55s for those graphs but please feel free to double check this)
-    
-    Damage control:
-    - Why didn't this work: using data from multiple papers, unclear what their ICs were, some constants undefined
-    - How are we validating? Need to find other data to validate curve shape against? Maybe what they got is actually wack and what we are getting actually make somes sense
-    - Double check figures/diagrams to ensure we are understanding all angles correctly
-    - We can also talk about the range fo the graph - ex. 0 <= activation <= 1, which means that the values we are receving technically make sense, but the shape of the curve is off
-        - Need to prove our model is somewhat realisitc :) 
-    """
--- main.py
+++ main.py
@@ -1,88 +1,76 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal
 from data import Data
 from ankle_model import FootDropAnkleModel
-import math
 
-
-def plot_data():
-    #### NOT BEING USED CURRENTLY - KIRA APR 1
-    A = np.asarray(Data.angle_data)
-    angle_rad = []
-    for val in A[:,1]:
-        angle_rad.append(val*(math.pi/180))
-
-    dt = np.diff(A[:,0])
-    dt_m = np.ma.array(dt, mask=(dt==0))
-    dy = np.diff(angle_rad)
-    dy_m = np.ma.array(dy, mask=(dy==0))
-    dydt = dy_m/dt_m
-    y = scipy.signal.savgol_filter(dydt, 25, 3)
-
-    d2t = np.diff(dt)
-    d2t_m = np.ma.array(d2t, mask=(d2t==0))
-    d2y = np.diff(dy)
-    d2y_m = np.ma.array(d2y, mask=(d2y==0))
-    d2yd2t = d2y_m/d2t_m
-    y2 = scipy.signal.savgol_filter(d2yd2t, 51, 3)
-    div_data = []
-    # for value in y2:
-    #     div_data.append(value/10000000)
-    plt.plot(A[12:len(d2t),0], y2[12:])
-    # plt.show()
-    plt.plot(A[12:len(dt),0], y[12:])
-
-    # plt.plot(A[12:len(d2t),0], div_data[12:])
-    plt.plot(A[12:,0], A[12:,1])
-    plt.show()
-
-    radius = 0.4
 
 
 def run_simulation():
+    """
+    Runs simulation for 0.35 s (swing phase). Plots solution (state vector).
+    """
     foot_drop = FootDropAnkleModel()
-    # foot_drop.simulate(4.0)
-    # foot_drop.simulate_rk4()
     soln = foot_drop.simulate(0.35)
     plt.plot(soln.t, soln.y[0], 'r')
+    plt.title('Muscle Activation vs. Time')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Activation Level')
     plt.show()
-    # print(foot_drop.muscle_ex_data)
     plt.plot(soln.t, soln.y[1], 'b')
+    plt.title('Absolute Orientation of Foot vs. Time')
+    plt.ylabel('Orientation (deg)')
+    plt.xlabel('Time (s)')
     plt.show()
     plt.plot(soln.t, soln.y[2], 'g')
-    plt.show() 
-    # print(soln.t)
+    plt.title('Absolute Rotational Velocity of Foot vs. Time')
+    plt.ylabel('Velocity (deg/s)')
+    plt.xlabel('Time (s)')
+    plt.show()
     print(soln.message)
     print(soln.status)
 
 def graph_data():
+    """
+    Graphs external state vector quantities and muscle excitation level. Can be used as a sanity
+    check to visually verify that data extracted from web plot digitizer matches the original 
+    graphs.
+    """
     x_ext1 = np.array(Data.linear_acc_shank_x)
     x_ext2 = np.array(Data.linear_acc_shank_z)
     x_ext3 = np.array(Data.abs_orientation_shank) 
     x_ext4 = np.array(Data.abs_velocity_rotation_shank)
     u1 = np.array(Data.muscle_excitation_level_fig3)
-    plt.plot(u1[:,0], u1[:,1])
-    # plt.plot(x_ext1[:,0], x_ext1[:,1], label='x-direction')
-    # plt.plot(x_ext2[:,0], x_ext2[:,1], label='z-direction')
-    # plt.title('Linear Acceleration of the Shank vs. Time')
-    # plt.xlabel('Time (s)')
-    # plt.ylabel('Acceleration (m/s^2)')
-    # plt.show()
-    # plt.plot(x_ext4[:,0], x_ext4[:,1])
-    # plt.title('Absolute Rotational Velocity of Shank vs. Time')
-    # plt.xlabel('Time (s)')
-    # plt.ylabel('Rotational Velocity (rad/s)')
-    # plt.show()
-    # plt.plot(x_ext3[:,0], x_ext3[:,1] *(math.pi/180))
-    # plt.title('Absolute Orientation of Shank vs. Time')
-    # plt.xlabel('Time (s)')
-    # plt.ylabel('Orientation (rad)')
+    plt.plot(u1[:, 0], u1[:, 1])
+    plt.title('Muscle Excitation vs. Time')
+    plt.ylabel('Muslce Excitation Level')
+    plt.xlabel('Time (s)')
+    plt.show()
+    plt.plot(x_ext1[:, 0], x_ext1[:, 1], label='x-direction')
+    plt.plot(x_ext2[:, 0], x_ext2[:, 1], label='z-direction')
+    plt.title('Linear Acceleration of the Shank vs. Time')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Acceleration (m/s^2)')
+    plt.legend()
+    plt.show()
+    plt.plot(x_ext4[:, 0], x_ext4[:, 1])
+    plt.title('Absolute Rotational Velocity of Shank vs. Time')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Rotational Velocity (rad/s)')
+    plt.show()
+    plt.plot(x_ext3[:, 0], x_ext3[:, 1])
+    plt.title('Absolute Orientation of Shank vs. Time')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Orientation (deg)')
     plt.show()
 
 def main():
+    """
+    Program starting point
+    """
     run_simulation()
-    # graph_data()
+    graph_data()
 
 
 if __name__ == '__main__':
