f1b6b050cfcfb0d7cf64f8c2fce6f78c9728c947
kirawadden <kirawa6@gmail.com>
2020-04-01 17:01:30 -0700

    further clean up

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from data import Data
 from ankle_model import FootDropAnkleModel
 
 
-
 def run_simulation():
     """
     Runs simulation for 0.35 s (swing phase). Plots solution (state vector).
@@ -19,17 +18,18 @@ def run_simulation():
     plt.ylabel('Activation Level')
     plt.show()
     plt.plot(soln.t, soln.y[1], 'b')
-    plt.title('Absolute Orientation of Foot vs. Time')
+    plt.title('Orientation of Foot vs. Time')
     plt.ylabel('Orientation (deg)')
     plt.xlabel('Time (s)')
     plt.show()
     plt.plot(soln.t, soln.y[2], 'g')
-    plt.title('Absolute Rotational Velocity of Foot vs. Time')
+    plt.title('Rotational Velocity of Foot vs. Time')
     plt.ylabel('Velocity (deg/s)')
     plt.xlabel('Time (s)')
     plt.show()
     print(soln.message)
     print(soln.status)
+
 
 def graph_data():
     """
@@ -37,11 +37,17 @@ def graph_data():
     check to visually verify that data extracted from web plot digitizer matches the original 
     graphs.
     """
+    # external state vector 1 -> linear acceleration of shank x-direction
     x_ext1 = np.array(Data.linear_acc_shank_x)
+    # external state vector 2 -> linear acceleration of shank z-direction
     x_ext2 = np.array(Data.linear_acc_shank_z)
+    # external state vector 3 -> orientation of shank 
     x_ext3 = np.array(Data.abs_orientation_shank) 
+    # external state vector 4 -> rotational velocity of shank
     x_ext4 = np.array(Data.abs_velocity_rotation_shank)
+    # input vector (u)
     u1 = np.array(Data.muscle_excitation_level_fig3)
+
     plt.plot(u1[:, 0], u1[:, 1])
     plt.title('Muscle Excitation vs. Time')
     plt.ylabel('Muslce Excitation Level')
@@ -55,24 +61,33 @@ def graph_data():
     plt.legend()
     plt.show()
     plt.plot(x_ext4[:, 0], x_ext4[:, 1])
-    plt.plot(x_ext4[6:15,0], x_ext4[6:15,1],'r', label='Swing Phase')
-    plt.title('Absolute Rotational Velocity of Shank vs. Time')
+    plt.title('Rotational Velocity of Shank vs. Time')
     plt.xlabel('Time (s)')
     plt.ylabel('Rotational Velocity (rad/s)')
     plt.legend()
     plt.show()
     plt.plot(x_ext4[:, 0], x_ext4[:, 1])
+    # partial swing phase
+    plt.plot(x_ext4[6:15,0], x_ext4[6:15,1],'r', label='Swing Phase')
+    plt.title('Rotational Velocity of Shank vs. Time (Partial Swing Phase)')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Rotational Velocity (rad/s)')
+    plt.legend()
+    plt.show()
+    plt.plot(x_ext4[:, 0], x_ext4[:, 1])
+    # full swing phase
     plt.plot(x_ext4[6:23,0], x_ext4[6:23,1],'r', label='Swing Phase')
-    plt.title('Absolute Rotational Velocity of Shank vs. Time')
+    plt.title(' Rotational Velocity of Shank vs. Time (Full Swing Phase)')
     plt.xlabel('Time (s)')
     plt.ylabel('Rotational Velocity (rad/s)')
     plt.legend()
     plt.show()
     plt.plot(x_ext3[:, 0], x_ext3[:, 1])
-    plt.title('Absolute Orientation of Shank vs. Time')
+    plt.title('Orientation of Shank vs. Time')
     plt.xlabel('Time (s)')
     plt.ylabel('Orientation (deg)')
     plt.show()
+
 
 def main():
     """
